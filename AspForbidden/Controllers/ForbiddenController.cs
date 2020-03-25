04a4ac7761b44172fdbb46ff73479771b6b6d1bf
Mikael Chudinov <mikael@chudinov.net>
2020-03-25 19:50:10 +0100

    Add project files.

---
+++ AspForbidden/Controllers/ForbiddenController.cs
@@ -0,0 +1,12 @@
+﻿namespace AspForbidden.Controllers
+{
+    using System.Net.Http;
+    public class ForbiddenController : System.Web.Http.ApiController
+    {
+        // GET: api/Forbidden
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateErrorResponse(System.Net.HttpStatusCode.Forbidden, "This call is not allowed!");
+        }
+    }
+}
